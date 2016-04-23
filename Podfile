f440461e5423fec1d34b31de15ee5e8af979dda1
Aaron Schubert <aaronschubert@Aarons-MBP.home>
2016-04-23 11:37:47 +0100

    Initial pass at rendering ASDK nodes from React Native

---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+# use_frameworks!
+
+target 'ReactAsyncly' do
+pod 'AsyncDisplayKit'
+pod 'React', :path => './node_modules/react-native', :subspecs => [
+  'Core',
+  'RCTImage',
+  'RCTNetwork',
+  'RCTText',
+  'RCTWebSocket',
+  # Add any other subspecs you want to use in your project
+]
+end
+
+target 'ReactAsynclyTests' do
+
+end
+
+target 'ReactAsynclyUITests' do
+
+end
--- ReactAsyncly/ViewController.m
+++ ReactAsyncly/ViewController.m
@@ -7,7 +7,8 @@
 //
 
 #import "ViewController.h"
-
+#import "RCTRootView.h"
+#import <AsyncDisplayKit/AsyncDisplayKit.h>
 @interface ViewController ()
 
 @end
@@ -15,13 +16,34 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+  [super viewDidLoad];
+  NSURL *jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios"];
+  // For production use, this `NSURL` could instead point to a pre-bundled file on disk:
+  //
+  //   NSURL *jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  //
+  // To generate that file, run the curl command and add the output to your main Xcode build target:
+  //
+  //   curl http://localhost:8081/index.ios.bundle -o main.jsbundle
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+                                                      moduleName: @"SimpleApp"
+                                               initialProperties:nil
+                                                   launchOptions:nil];
+  
+  [self.view addSubnode:rootView];
+  rootView.frame = self.view.bounds;
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewWillLayoutSubviews
+{
+  [super viewWillLayoutSubviews];
+  
 }
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
 }
 
 @end
